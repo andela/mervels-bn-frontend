d22a86f53a3de0a4fb9d193695ff5b1fb7e91ee0
Davis Kabiswa <daviskabiswa@Daviss-MacBook-Pro.local>
2019-11-04 12:01:05 +0200

    chore(redux): set-up redux for the project [Starts #168131515]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './redux/store';
 import './App.css';
 
-function App() {
+const store = configureStore();
+
+const App = () => {
   return (
-    <div className="App">
-      BarefootNomad frontEnd
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        BarefootNomad frontEnd
+      </div>
+    </Provider>
   );
-}
+};
 
 export default App;
