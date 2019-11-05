5501138ef2e0b4a034144fd711fe99f88bcb024b
Jonathan Shyaka <jonashyaka2@gmail.com>
2019-11-05 12:58:58 +0200

    chore(error): set up error logger for the project
    - Added sentry to track all our errors
    
    [starts #169502584]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import './index.css';
 import App from './App';
+import { SENTRY_DSN } from '../config';
+
+Sentry.init({ dsn: SENTRY_DSN });
 
 ReactDOM.render(<App />, document.getElementById('root'));
