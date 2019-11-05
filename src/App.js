a0effa344eed9da48fb7aa83bd909ea16c51209e
willywax <williamtrans2011@gmail.com>
2019-11-05 12:44:46 +0200

    chore(sass): set up sass loaded
    add node-sass to dependencies
    
    [Finishes #168131519]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
-import './App.css';
+import './App.scss';
 
 const store = configureStore();
 
