257c589d9b2feb3f1503950ca6fbb46e0be4e7b7
BAHATI Robben <lux@BAHATIs-MacBook-Pro.local>
2019-11-05 17:19:52 +0200

    chore(storybook): setting up storybook
    - setting up storybook for easy documentation of our components
    -  includes all tools meeded to get started writing stories
    
    [Starts #168131522]

---
+++ .storybook/barefootTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Barefoot Nomad',
+    brandImage: 'marvel_logo.png',
+    colorSecondary: '#1976D2'
+})
