70819a9b8ab5b9f60d7f3aa739e0c27870bb0f40
willywax <williamtrans2011@gmail.com>
2019-11-26 09:33:39 +0200

    - fix notifications when readone clicked

--- src/components/shared/navbarComponent.js
+++ src/components/shared/navbarComponent.js
@@ -1,9 +1,11 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable import/no-named-as-default */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable no-useless-constructor */
 /* eslint-disable react/prefer-stateless-function */
 import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import ChatIcon from "@material-ui/icons/Chat";
 import useStyles from "./iconStyles";
@@ -12,7 +14,7 @@ import NotificationPane from "../NotificationPane";
 import logo from "../../logo/logo-long.png";
 
 
-const Navbar = () => {
+const Navbar = ({history}) => {
   const classes = useStyles();
   const [showPane, setShowPane] = useState('notification hide');
 
@@ -49,8 +51,8 @@ const Navbar = () => {
           </li>
         </ul>
       </nav>
-      <NotificationPane classes={showPane} handlePane={togglePane} />
+      <NotificationPane classes={showPane} handlePane={togglePane} history={history} />
     </>
   );
 };
-export default Navbar;
+export default withRouter(Navbar);
