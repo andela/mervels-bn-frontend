f3904d73b0a7a137f418e76ff7ae9f59f45887dd
Kabiswa Davis <kabiswad@gmail.com>
2019-12-11 20:16:34 +0200

    ch(styling): refactor css of 500 and 404 pages (#99)

--- src/components/500Page.js
+++ src/components/500Page.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 function ServerErrorPage() {
-    return(
-        <>
-            <h2>500</h2>
-            <h4>Server Error</h4>
-            <p>Problem Occured Try Again OR </p>
-            <p>Check Your Internet Connection</p>
-        </>
-    );
+    return (
+        <div className="c4v-contianer">
+<div className="c4v">
+        <img alt="barefootNomad Logo" className="barefoot-logo" src="https://res.cloudinary.com/bahati/image/upload/v1573114920/marvel_logo_fngq4h.png"/>
+        <div className="call4verif-container">
+            <h3 className="c4v-title">SERVER ERROR</h3>
+            <h1 className="error-num">500</h1>
+            <p>Problem Occured Try Again OR Check Your Internet Connection</p>
+        </div>
+        </div>
+        </div>
+);
 }
 
 export default ServerErrorPage;
