9ff865116b2da9b2211106bfd3650c1536e118bf
BAHATI Robben <lux@BAHATIs-MacBook-Pro.local>
2019-12-12 13:21:38 +0200

    creating stories for different features

---
+++ src/stories/index.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Button, Welcome } from '@storybook/react/demo';
+import App from '../App';
+import '../App.scss';
+
+
+storiesOf('Home page | Main app/Applicaton', module)
+    .add('welcome', () => (<Welcome/>))
+    .add('home page', () => (<App/>));
+    
+storiesOf('Building blocks | Button', module)
+    .add('Non lickable', () => (<Button>Non clickable</Button>))
+    .add('clickable', () => (<Button onClick={action('clicked')}>Click Me</Button>));
